c2184ff822c5de48eec023e453668fa1de4df0b4
Rakesh <rsahu@inquesttechnologies.com>
2017-03-30 22:19:32 +0530

    added page-not-found generic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {
+  RouterModule,
+  PreloadAllModules
+} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './app.pagenotfound.component';
+import { routes } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes, { useHash: true, preloadingStrategy: PreloadAllModules })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.pagenotfound.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-page-notfound',
+    templateUrl: './app.pagenotfound.component.html',
+    styleUrls: ['./app.pagenotfound.component.css']
+})
+export
+    class PageNotFoundComponent {
+    constructor() {
+
+    }
+}
